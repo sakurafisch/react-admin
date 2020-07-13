5b14317ee71d0bd7b9e3bc41e77f0342028e37dc
winnerwinter1998 <winnerwinter1998@gmail.com>
2020-07-13 19:34:52 +0800

    添加路由

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route, HashRouter } from 'react-router-dom';
 import './App.css';
 
-function App() {
+import Home from './views/Home'
+import About from './views/About'
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Switch>
+        <Route component={Home} exact path="/" />
+        <Route component={About} path="/about" />
+      </Switch>
+    </HashRouter>
   );
 }
-
-export default App;
